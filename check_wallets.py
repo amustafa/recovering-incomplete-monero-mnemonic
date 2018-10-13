dd55bf242822d068d3331b1fdcb7ceb72c317757
Adam Mustafa <amustafa2011@gmail.com>
2018-10-13 16:50:51 -0400

    restructured to make what I did a little clearer and made it python 3 compatible

--- check_wallets.py
+++ check_wallets.py
@@ -6,11 +6,6 @@ Uses myMonero.com to check the
 import json
 import requests
 
-with open("output/myMoneroAddressKeySets.json", 'r') as addresses_fp:
-    address_key_sets = json.load(addresses_fp)
-
-with open("output/discovered_seeds.json", 'r') as seeds_fp:
-    mnemonics = json.load(seeds_fp)
 
 mymonero_request_header = {
     'origin': 'https://mymonero.com',
@@ -22,19 +17,42 @@ mymonero_request_header = {
     'authority': 'api.mymonero.com:8443',
 }
 
-for key_set_i in range(len(address_key_sets)):
-    key_set = address_key_sets[key_set_i]
 
-    data = '{"address":"%s","view_key":"%s"}' % (key_set[0], key_set[1])
-    resp = requests.post('https://api.mymonero.com:8443/get_address_info', headers=mymonero_request_header, data=data)
-    try:
-        if int(json.loads(resp.content)["total_received"]) > 0:
-            print "Discovered Used Wallet"
-            print "Key Set:", key_set
-            print "Mnemonic to use: ", mnemonics[key_set_i]
-    except KeyError as e:
-        if resp.status_code == 500:
-            pass
-        else:
-            print "Last keyset tried: ", key_set_i, key_set
-            raise
+def lookup_address_on_my_monero(address, view_key):
+
+    data = '{"address":"%s","view_key":"%s"}' % (address, view_key)
+    resp = requests.post('https://api.mymonero.com:8443/get_address_info',
+                         headers=mymonero_request_header, data=data)
+
+    return resp
+
+
+def get_stored_mnemonic(index):
+    with open("output/discovered_seeds.json", 'r') as seeds_fp:
+        mnemonics = json.load(seeds_fp)
+        return mnemonics[index]
+
+
+def get_stored_keys():
+    with open("output/myMoneroAddressKeySets.json", 'r') as addresses_fp:
+        address_key_sets = json.load(addresses_fp)
+    return address_key_sets
+
+
+if __name__ == "__main__":
+    address_key_sets = get_stored_keys()
+
+    for key_set_i, key_set in enumerate(address_key_sets):
+        resp = lookup_address_on_my_monero(key_set[0], key_set[1])
+
+        try:
+            if int(json.loads(resp.content)["total_received"]) > 0:
+                print("Discovered Used Wallet")
+                print("Key Set:", key_set)
+                print("Mnemonic to use: ", get_stored_mnemonic(key_set_i))
+        except KeyError as e:
+            if resp.status_code == 500:
+                pass
+            else:
+                print("Last keyset tried: ", key_set_i, key_set)
+                raise
